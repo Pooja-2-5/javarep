d4a6569b84f46efaa8db3b7ef7ad93e644ca2cdc
Pooja-2-5 <poojap.mca25@siescoms.sies.edu.in>
2026-02-23 12:18:13 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,8 @@
 import java.util.*;
 public class hello{
     public static void main(String[] args){
-        System.out.println("Hello World");
+       System.out.println("hello world");
         System.out.println("Addition:" + (2 + 3));
+        System.out.println("byye");
     }
 }
